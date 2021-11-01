756259b4b955468cb43de148df6c7908b3be4494
Ramon Honorio <ramon.honorio@nubank.com.br>
2021-11-01 11:49:44 -0300

    Add Factory Method conceptual examples

--- creational-patterns/factory-method/cli/bin/cli.dart
+++ creational-patterns/factory-method/cli/bin/cli.dart
@@ -1,9 +1,10 @@
 import 'package:cli/conceptual_example/creator.dart';
 
 void main(List<String> arguments) {
-  final creatorA = ConcreteCreatorA();
-  final creatorB = ConcreteCreatorB();
-
-  Client.someClientCode(creatorA);
-  Client.someClientCode(creatorB);
+  for (var creator in [
+    ConcreteCreatorA(),
+    ConcreteCreatorB(),
+  ]) {
+    Client.someClientCode(creator);
+  }
 }
